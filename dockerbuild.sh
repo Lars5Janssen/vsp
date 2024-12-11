c2b7b548791d5c6109b7e025d94b5f9b5c4e73f2
Lars Janssen <lars.e.janssen@gmail.com>
2024-12-11 17:07:02 +0100

    schoenheiten

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,6 +5,5 @@ if test -f "$FILE"; then
     cp /usr/src/app/main /usr/local/bin/app
 else
     echo "found OWN compiled binary" > /app/logs/compile.log
-    ls -la
     go build -v -o /usr/local/bin/app main.go
 fi
